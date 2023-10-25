2c60e1a746ff778fb76a77a8ff37169b9028bd74
Planxnx <planxthanee@gmail.com>
2023-10-25 19:18:26 +0700

    docs: add go-reference doc

---
+++ doc.go
@@ -0,0 +1,26 @@
+// Copyright 2023 Cleverse. All rights reserved.
+// Use of this source code is governed by a MIT License
+// license that can be found in the LICENSE file.
+
+/*
+Package go-utilities provides a miscellaneous useful shared Go packages by [Cleverse].
+
+# Packages
+
+  - [utils]: Minimalist pure Golang optimized generic utilities for Cleverse projects.
+  - [errors]: Minimalist and zero-dependency errors library with stacktrace support for Go (for wrapping and formatting an errors).
+  - [queue]: Minimalist and zero-dependency low-level and simple queue library for thread-safe and unlimited-size generics in-memory message queue library fo Go (async enqueue and blocking dequeue supports).
+    The alternative way to communicate between goroutines compared to `channel`
+  - [address]: High efficient and minimal utilities library that will help you to work with Ethereum addresses easier. (a [go-ethereum] helper library)
+  - [fixedpoint]: A [shopspring/decimal] wrapper library for fixed point arithmetic operations in Cleverse projects.
+
+[Cleverse]: https://tip.golang.org/doc/comment#links
+[go-ethereum]: https://github.com/ethereum/go-ethereum
+[shopspring/decimal]: https://pkg.go.dev/github.com/shopspring/decimal
+[utils]: https://pkg.go.dev/github.com/Cleverse/go-utilities/utils
+[errors]: https://pkg.go.dev/github.com/Cleverse/go-utilities/errors
+[queue]: https://pkg.go.dev/github.com/Cleverse/go-utilities/queue
+[address]: https://pkg.go.dev/github.com/Cleverse/go-utilities/address
+[fixedpoint]: https://pkg.go.dev/github.com/Cleverse/go-utilities/fixedpoint
+*/
+package goutilities
